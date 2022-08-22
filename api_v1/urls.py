972fd4d959812027507bf82d9d65cd45ab0f5a06
Jefferson Silva <jeffsilva1@outlook.com>
2022-08-22 12:41:17 -0300

    Update api_v1/urls.py

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+
+router.register(r'user/config', views.UserConfigView, basename='user_config')
+
 urlpatterns = [
-    path('', views.index)
+    path('', include(router.urls)),
 ]
